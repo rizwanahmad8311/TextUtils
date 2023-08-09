fe5c2b81894267923b62711f27448f9368025c97
rizwan737 <89721796+rizwan737@users.noreply.github.com>
2023-08-09 16:14:52 +0500

    fixing issues words count,cummulative layout shift

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
+    
     const HandleUpperCase = () => {
         if (text) {
             let Upper = text.toUpperCase()
@@ -81,19 +82,19 @@ export default function TextForm(props) {
                     <label htmlFor="mybox" className="form-label">{props.title}</label>
                     <textarea className="form-control" id="mybox" value={text} onChange={HandleOnChange} style={{ backgroundColor: props.mode === `light` ? `white` : `#042730`, color: props.mode === `dark` ? `white` : `black`, placeholderColor: 'red' }} placeholder={props.placeholder} rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={HandleUpperCase} >Convert to UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={HandleLowerCase} >Convert to LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={HandleClearText} >Clear Text</button>
-                <button className="btn btn-primary mx-1" onClick={HandleUndoText} >Undo Text</button>
-                <button className="btn btn-primary mx-1" onClick={HandleCopyText} >Copy Text</button>
-                <button className="btn btn-primary mx-1" onClick={HandleExtraSpaces} >Remove Extra Spaces</button>
+                <button disabled={text.replace(/\s+/g, '').length===0} className="btn btn-primary mx-1 my-1" onClick={HandleUpperCase} >Convert to UpperCase</button>
+                <button disabled={text.replace(/\s+/g, '').length===0} className="btn btn-primary mx-1 my-1" onClick={HandleLowerCase} >Convert to LowerCase</button>
+                <button disabled={text.replace(/\s+/g, '').length===0} className="btn btn-primary mx-1 my-1" onClick={HandleClearText} >Clear Text</button>
+                <button  className="btn btn-primary mx-1 my-1" onClick={HandleUndoText} >Undo Text</button>
+                <button disabled={text.replace(/\s+/g, '').length===0} className="btn btn-primary mx-1 my-1" onClick={HandleCopyText} >Copy Text</button>
+                <button disabled={text.replace(/\s+/g, '').length===0} className="btn btn-primary mx-1 my-1" onClick={HandleExtraSpaces} >Remove Extra Spaces</button>
             </div>
             <div className="container" style={{ color: props.mode === `dark` ? `white` : `black` }}>
                 <h1>Text Summary</h1>
-                <p>{text.split(" ").length - 1} words and {text.length} Characters</p>
-                <p>{0.008 * text.split(" ").length} minutes to read</p>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.replace(/\s+/g, '').length} Characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
                 <h2>Preview</h2>
-                <p>{text.length > 0 ? text : "Enter Something above in the text box to preview"}</p>
+                <p>{text.length > 0 ? text : "Nothing to Preview"}</p>
             </div>
         </>
     )
